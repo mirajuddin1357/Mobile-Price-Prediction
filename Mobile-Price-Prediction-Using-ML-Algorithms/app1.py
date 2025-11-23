e3853d39bf5c97f13f5fea31d92bf3f91c508fee
Miraj Us Din <162776773+mirajuddin1357@users.noreply.github.com>
2025-11-23 13:41:15 +0500

    Update app1.py

--- Mobile-Price-Prediction-Using-ML-Algorithms/app1.py
+++ Mobile-Price-Prediction-Using-ML-Algorithms/app1.py
@@ -1,18 +1,13 @@
 import os
 import pickle
 import pandas as pd
+from PIL import Image
+import streamlit as st
 
-# Define the base directory and file paths
-BASE_DIR = '/mnt/data'
-csv_path = os.path.join(BASE_DIR, "smartphone_cleaned_v1.csv")
-pkl_path = os.path.join(BASE_DIR, "smartphone_price_model.pkl")
+BASE_DIR = os.path.dirname(__file__)
 
-# Load CSV
-df = pd.read_csv(csv_path)
-
-# Load Pickle model
-with open(pkl_path, "rb") as f:
+df = pd.read_csv(os.path.join(BASE_DIR, "smartphone_cleaned_v1.csv"))
+with open(os.path.join(BASE_DIR, "smartphone_price_model.pkl"), "rb") as f:
     model = pickle.load(f)
-
-# Show outputs
-df.head(), type(model)
+img = Image.open(os.path.join(BASE_DIR, "image.png"))
+st.image(img, width=600)
